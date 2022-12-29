3753705a42da12656a50a8e6a5af7ac3b7f45cc7
dragonmux <git@dragonmux.network>
2022-12-29 15:12:02 +0000

    frameReassembly: Implemented a type for handling a frame fragment's metadata

--- src/frameReassemblyInternal.hxx
+++ src/frameReassemblyInternal.hxx
@@ -4,9 +4,34 @@
 
 #include <cstdint>
 
+extern "C"
+{
+#include <epan/reassemble.h>
+}
+
 namespace sol::frameReassembly::internal
 {
 	static int32_t solAnalyzerFrameProtocol{-1};
+
+	struct frameFragment_t final
+	{
+		uint32_t totalLength;
+		uint32_t length;
+		uint32_t frameNumber;
+		uint32_t *frameNumberPtr;
+
+		frameFragment_t(const uint32_t packetLength, const uint32_t len, const uint32_t frameNum) noexcept :
+			totalLength{packetLength}, length{len}, frameNumber{frameNum},
+			frameNumberPtr
+			{
+				[](const uint32_t frameNumber)
+				{
+					auto *const result = g_new0(uint32_t, 1);
+					*result = frameNumber;
+					return result;
+				}(frameNum)
+			} { }
+	};
 } // namespace sol::frameReassembly::internal
 
 #endif /*SOL_PACKET_DISSECTOR_INTERNAL_HXX*/
